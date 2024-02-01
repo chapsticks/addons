883fe4dda0c3bbbe220501d31d7f608f10078dad
Shichao.Zhou <396564168@qq.com>
2024-02-01 15:36:33 +0800

    Update __manifest__.py

--- db_constraint/__manifest__.py
+++ db_constraint/__manifest__.py
@@ -1,9 +1,9 @@
 {
     'name': '物理外键拓展',
     'summary': '支持物理外键的建立和解除',
-    'description': '支持物理外键的建立和解除，dtcloud默认所有外键都会在数据库层面进行外键约束的建立，该模块将orm与数据层的物理外键解耦，同时保有orm的外键关联查询可用',
-    'website': 'http://www.dtcloud360.com/',
-    'category': '新城建/通用工具',
+    'description': '支持物理外键的建立和解除，odoo默认所有外键都会在数据库层面进行外键约束的建立，该模块将orm与数据层的物理外键解耦，同时保有orm的外键关联查询可用',
+    'website': '',
+    'category': 'base',
     'depends': [],
     'data': [
     ],
