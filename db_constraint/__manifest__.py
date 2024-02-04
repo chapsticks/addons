8591d4c84a56948a8ae0ce809bb7f2a509b53a3c
Shichao.Zhou <396564168@qq.com>
2024-02-04 17:11:23 +0800

    Update __manifest__.py

--- db_constraint/__manifest__.py
+++ db_constraint/__manifest__.py
@@ -3,7 +3,7 @@
     'summary': '支持物理外键的建立和解除',
     'description': '支持物理外键的建立和解除，odoo默认所有外键都会在数据库层面进行外键约束的建立，该模块将orm与数据层的物理外键解耦，同时保有orm的外键关联查询可用',
     'website': '',
-    'author': "周士超",
+    'author': "周士超（396564168@qq.com）",
     'version': '1.0',
     'category': 'base',
     'depends': [],
